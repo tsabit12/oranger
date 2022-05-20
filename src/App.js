beaea0ce6b91b0a79ee87c69ac095559dcf4ff1b
tsabit12 <tsabit830@gmail.com>
2022-05-20 23:00:13 +0700

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { ThemeProvider } from "@mui/material/styles";
+import { Provider as ReduxProvider } from "react-redux";
 import { HashRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
 import Routes from "./Routes";
 import theme from "./theme";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 function App() {
   return (
     <HashRouter>
-      <ThemeProvider theme={theme}>
-        <Routes />
-      </ThemeProvider>
+      <ReduxProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      </ReduxProvider>
     </HashRouter>
   );
 }
